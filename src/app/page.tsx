4870f9bf36bdd100adb66fdd0263b8b22d5872b4
JanChoc <jan.choc@h-vision.cz>
2026-01-16 03:36:10 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../lib/supabase;"
 
 type Shift = { id: string; start_at: string; end_at: string | null };
 
